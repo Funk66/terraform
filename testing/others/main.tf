a47496036feb45f8af651fbe440fe0323d376e5c
Guillermo Guirao Aguilar <gga@kialo.com>
2020-11-19 09:31:31 +0100

    Added main and provider

---
+++ testing/others/main.tf
@@ -0,0 +1,7 @@
+terraform {
+  backend "remote" {
+    organization = "kialo"
+  }
+
+  required_version = ">=0.12"
+}
---
+++ testing/others/provider.tf
@@ -0,0 +1,11 @@
+variable "testing_account_id" {}
+variable "principal_account_id" {}
+
+provider "aws" {
+  version             = "2.42.0"
+  region              = "eu-central-1"
+  allowed_account_ids = [var.testing_account_id]
+  assume_role {
+    role_arn = "arn:aws:iam::${var.testing_account_id}:role/admin"
+  }
+}
